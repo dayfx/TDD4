20de52ee291a99442018d5e6d5a53c59993aaf4a
dayfx <if22b206@technikum-wien.at>
2024-06-02 13:25:11 +0200

    Sell date passed - quality degrades twice as fast TEST PASSED

---
+++ spec/gilded_rose_spec.js
@@ -0,0 +1,31 @@
+describe("Gilded Rose", function() {
+
+  it("General Chainvest test to make sure stats update as intended", function() {
+    let days = 5;
+    items = [ new Item("+5 Dexterity Vest", 10, 20) ];
+    
+    while(days != 0){
+      update_quality();
+      days--;
+    }
+
+    expect(items[0].name).toEqual("+5 Dexterity Vest");
+    expect(items[0].sell_in).toEqual(5);
+    expect(items[0].quality).toEqual(15);
+  });
+
+  it("Once the sell date has passed, quality degrades twice as fast", function() {
+    let days = 12; //days over sell_in date
+    items = [ new Item("+5 Dexterity Vest", 10, 20) ];
+    
+    while(days != 0){
+      update_quality();
+      days--;
+    }
+
+    expect(items[0].name).toEqual("+5 Dexterity Vest");
+    expect(items[0].sell_in).toEqual(-2);
+    expect(items[0].quality).toEqual(6); //decreases by 2 instead of 1 for 2 each of the 2 days over sell_in date
+  });
+
+});
