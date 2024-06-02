99b970a8524511638b2779ced1c2a691bbefb892
dayfx <if22b206@technikum-wien.at>
2024-06-02 16:42:06 +0200

    Refactored, two versions, all tests green. Second version, Info gained from AI as to how to structure code better within methods in order to make code concise and readable

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -18,52 +18,54 @@ function update_quality() {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) { // Quality never drops to negative
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1 //
+          items[i].quality-- //
           if(items[i].name == 'Conjured Mana Cake' && items[i].quality > 0){ //if item is conjured and quality still not 0
-            items[i].quality = items[i].quality - 1 //item quality degrades twice as fast (similar code down below for expired sell_in date) (only works for conjured mana cake in this case)
+            items[i].quality-- //item quality degrades twice as fast (similar code down below for expired sell_in date) (only works for conjured mana cake in this case)
           }
         }
       }
     } else {
       if (items[i].quality < 50) { // Quality never goes above 50
-        items[i].quality = items[i].quality + 1
+        items[i].quality++
         if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {                  // backstage pass quality update .
-            if (items[i].quality < 50) {                //                               .
-              items[i].quality = items[i].quality + 1   //                               .
-            }                                           //                               .
-          }                                             //                               .
-          if (items[i].sell_in < 6) {                   //                               .
-            if (items[i].quality < 50) {                //                               .
-              items[i].quality = items[i].quality + 1   //            definitely refactor
+          if (items[i].quality < 50){
+            if (items[i].sell_in < 11) {                          
+              items[i].quality++                                       
+            }                                        
+            if (items[i].sell_in < 6) {       
+              items[i].quality++                        
+            }
+            if (items[i].quality > 50) {
+              items[i].quality = 50;
             }
           }
         }
       }
     }
     if (items[i].name != 'Sulfuras, Hand of Ragnaros') { // Sulfuras never decreases in Quality or Date
-      items[i].sell_in = items[i].sell_in - 1;
+      items[i].sell_in--;
     }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) { //for "normal" items, decreases twice as fast in quality, but still cant go negative
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-              if(items[i].name == 'Conjured Mana Cake' && items[i].quality > 0){ //if item is conjured and quality still not 0
-                items[i].quality = items[i].quality - 1 //item quality degrades twice as fast (needs to be implemented here too in case theres ever anitem thats)
-                                                        //conjured with higher sell_in dates and quality stats) (again, only conjured mana cake here)
-              }
+    if(items[i].sell_in < 0) { // if expired
+      if(items[i].name == 'Aged Brie') {
+        if(items[i].quality < 50) {
+          items[i].quality++; // Brie quality increases twice as fast
+        } 
+      } else if(items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+        items[i].quality = 0; // backstage passes drop to quality 0 after the concert
+      } else if(items[i].name == 'Conjured Mana Cake') {
+          if(items[i].quality > 0) { // technically never matters here, because quality cant go negative and sell_in dates and quality too low on conjured cake,
+                                     // but technically for future other conjured items, this might become necessary to make sure double degradation is accounted for here too
+            items[i].quality - 2;
+            if (items[i].quality < 0) {
+              items[i].quality = 0; //so quality cant go negative
             }
-          }
-        } else { //ticket quality drops to 0
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else { //else Brie also starts INCREASING in quality twice as fast apparently
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
+        } 
+      } else {
+        items[i].quality--;
+        if(items[i].quality < 0) {
+          items[i].quality = 0;
+        } // normal items degrade twice as fast when expired
       }
-    }
+  }
   }
 }
